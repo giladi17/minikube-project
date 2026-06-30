9659ee387746aa224fae09cb5b5fcfd3a0118797
Giladi17 <giladi17@gmail.com>
2026-06-30 14:17:36 +0300

    fix: updated image tag to resolve ImagePullBackOff11

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ app.post('/shorten', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running and listening on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running and listening on port ${PORT}`);
 });
